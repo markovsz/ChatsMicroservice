de411fc6aa6237b6eacbe60313f54509db86a977
Stas <markovszzzz@gmail.com>
2022-06-27 00:51:10 +0300

    extract user id filter added

---
+++ ChatsMicroservice/Filters/ExtractUserIdFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ChatsMicroservice.Filters
+{
+    public class ExtractUserIdFilter : IActionFilter
+    {
+        public ExtractUserIdFilter()
+        {
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var action = context.RouteData.Values["action"];
+            var controller = context.RouteData.Values["controller"];
+            var userIdClaim = context.HttpContext.User.Claims.Where(c => c.Value.Equals(ClaimTypes.NameIdentifier)).FirstOrDefault();
+            context.ActionArguments.Add("userId", userIdClaim.Value); /*!*/
+        }
+    }
+}
