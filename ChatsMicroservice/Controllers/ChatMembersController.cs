5671686099e5ec77f36dacb649f839787d2b3f95
Stas <markovszzzz@gmail.com>
2022-06-27 00:49:09 +0300

    chatmembers and messages controllers added

---
+++ ChatsMicroservice/Controllers/ChatMembersController.cs
@@ -0,0 +1,51 @@
+﻿using Messager.Chats.Application.Services.DataTransferObjects;
+using Messager.Chats.Infrastructure.Services.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatsMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatMembersController : ControllerBase
+    {
+        private ChatMembersService _chatMembersService;
+
+        public ChatMembersController(ChatMembersService chatMembersService)
+        {
+            _chatMembersService = chatMembersService;
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> AddChatMemberAsync(ChatMemberForCreateDto chatMember)
+        {
+            var chatMemberId = await _chatMembersService.AddChatMemberAsync(chatMember);
+            return CreatedAtRoute("GetChatMember", new { id = chatMemberId });
+        }
+
+        [HttpGet("chat/{chatId}")]
+        public async Task<IActionResult> GetChatMembersAsync(Guid chatId)
+        {
+            var chatMembers = await _chatMembersService.GetChatMembersAsync(chatId);
+            return Ok(chatMembers);
+        }
+
+        [HttpGet("/chat/{chatId}/member/{userId}", Name = "GetChatMember")]
+        public async Task<IActionResult> GetChatMemberByUserIdAsync(Guid chatId, Guid userId, bool trackChanges)
+        {
+            var chatMember = await _chatMembersService.GetChatMemberByUserIdAsync(chatId, userId);
+            return Ok(chatMember);
+        }
+
+        [HttpDelete("/chat/{chatId}/member/{userId}")]
+        public async Task<IActionResult> DeleteChatMemberByUserIdAsync(Guid chatId, Guid userId)
+        {
+            await _chatMembersService.DeleteChatMemberByUserIdAsync(chatId, userId);
+            return NoContent();
+        }
+    }
+}
---
+++ ChatsMicroservice/Controllers/MessagesController.cs
@@ -0,0 +1,65 @@
+﻿using Messager.Chats.Application.Services.DataTransferObjects;
+using Messager.Chats.Application.Services.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatsMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private IMessagesService _messagesService;
+
+        public MessagesController(IMessagesService messagesService)
+        {
+            _messagesService = messagesService;
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> CreateMessageAsync(MessageForCreateDto messageDto)
+        {
+            var messageId = await _messagesService.CreateMessageAsync(messageDto);
+            return CreatedAtRoute("GetMessage", new { id = messageId });
+        }
+
+        [HttpGet("chat/{chatId}/member/{userId}")]
+        public async Task<IActionResult> GetUserMessagesFromChatAsync(Guid userId, Guid chatId)
+        {
+            var messages = await _messagesService.GetUserMessagesFromChatAsync(userId, chatId);
+            return Ok(messages);
+        }
+
+        [HttpGet("chat/{chatId}")]
+        public async Task<IActionResult> GetChatMessagesAsync(Guid chatId)
+        {
+            var messages = await _messagesService.GetChatMessagesAsync(chatId);
+            return Ok(messages);
+        }
+
+        [HttpGet("{messageId}", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessageByIdAsync(Guid messageId)
+        {
+            var message = await _messagesService.GetMessageByIdAsync(messageId);
+            return Ok(message);
+        }
+
+        [HttpPut("{messageId}")]
+        public IActionResult UpdateMessage(Guid messageId, MessageForUpdateDto messageDto)
+        {
+            _messagesService.UpdateMessage(messageId, messageDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{messageId}")]
+        public async Task<IActionResult> DeleteMessageByIdAsync(Guid messageId)
+        {
+            await _messagesService.DeleteMessageByIdAsync(messageId);
+            return NoContent();
+        }
+    }
+}
