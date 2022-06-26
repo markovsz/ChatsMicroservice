751f9ebb0c5ae4fb3c0c3f556f0565f793b9fa3b
Stas <markovszzzz@gmail.com>
2022-06-27 00:53:20 +0300

    all the services was added to di container

---
+++ ChatsMicroservice/ServiceExtentions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatsMicroservice.Filters;
+using Messager.Chats.Application.Services.Services;
+using Messager.Chats.Domain.Interfaces.Repositories;
+using Messager.Chats.Domain.Interfaces.Repositories.Repository;
+using Messager.Chats.Infrastructure.Data;
+using Messager.Chats.Infrastructure.Data.Repository;
+using Messager.Chats.Infrastructure.Services.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatsMicroservice
+{
+    public static class ServiceExtentions
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), 
+                b => b.MigrationsAssembly("TelephoneNetworkProvider")));
+        }
+
+        public static void ConfigureRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IChatsRepository, ChatsRepository>();
+            services.AddScoped<IChatMembersRepository, ChatMembersRepository>();
+            services.AddScoped<IMessagesRepository, MessagesRepository>();
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        }
+
+        public static void ConfigureService(this IServiceCollection services)
+        {
+            services.AddScoped<IChatsService, ChatsService>();
+            services.AddScoped<IChatMembersService, ChatMembersService>();
+            services.AddScoped<IMessagesService, MessagesService>();
+        }
+
+        public static void ConfigureActionFilters(this IServiceCollection services)
+        {
+            services.AddScoped<ExtractUserIdFilter>(); //transient
+        }
+    }
+}
--- ChatsMicroservice/Startup.cs
+++ ChatsMicroservice/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +13,21 @@ namespace ChatsMicroservice
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureDbContext(_configuration);
+            services.ConfigureRepository();
+            services.ConfigureService();
+            services.ConfigureActionFilters();
             services.AddControllers();
         }
 
