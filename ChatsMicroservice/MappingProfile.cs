a5907799469358ec7f690cacf77059be0645a0ec
Stas <markovszzzz@gmail.com>
2022-07-17 11:30:04 +0300

    mapping profile was changed due to changes in dto's

--- ChatsMicroservice/MappingProfile.cs
+++ ChatsMicroservice/MappingProfile.cs
@@ -1,20 +1,20 @@
 ﻿using AutoMapper;
 using Messager.Chats.Application.Services.DataTransferObjects;
 using Messager.Chats.Domain.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ChatsMicroservice
+namespace Messager.Chats.API
 {
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
             CreateMap<Chat, ChatForReadDto>();
-            CreateMap<ChatForCreateDto, Chat>();
-            CreateMap<ChatForUpdateDto, Chat>();
+            CreateMap<ChatForCreateDto, Chat>()
+                .ForMember(c => c.IsPrivate, c => c.Ignore())
+                .BeforeMap((cd, c) => c.IsPrivate = (cd.IsPrivate.Equals("true") ? true : false));
+            CreateMap<ChatForUpdateDto, Chat>()
+                .ForMember(c => c.IsPrivate, c => c.Ignore())
+                .BeforeMap((cd, c) => c.IsPrivate = (cd.IsPrivate.Equals("true") ? true : false));
 
             CreateMap<ChatMember, ChatMemberForReadDto>();
             CreateMap<ChatMemberForCreateDto, ChatMember>();
