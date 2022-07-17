0e6103f15abd8b999e13594e0a0c6443b018afa6
Stas <markovszzzz@gmail.com>
2022-07-17 11:34:36 +0300

    related entities controller was added

---
+++ ChatsMicroservice/Controllers/RelatedEntitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Messager.Chats.API.Filters;
+using Messager.Chats.Application.Services.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Messager.Chats.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelatedEntitiesController : ControllerBase
+    {
+        IChatMembersService _chatMembersService;
+
+        public RelatedEntitiesController(IChatMembersService chatMembersService)
+        {
+            _chatMembersService = chatMembersService;
+        }
+
+
+        [Authorize(Roles = "Administrator,Customer")]
+        [ServiceFilter(typeof(ExtractUserIdFilter))]
+        [HttpDelete("")]
+        public async Task<IActionResult> DeleteUserRelatedEntities(Guid userId)
+        {
+            await _chatMembersService.DeleteChatMembersByUserIdAsync(userId);
+            return NoContent();
+        }
+    }
+}
