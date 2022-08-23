8214f7a3c8d615cac4d242af3fb71216378d777e
Stas <markovszzzz@gmail.com>
2022-08-23 18:49:02 +0300

    sender name property was removed

--- Messager.Chats.Application.Services/DataTransferObjects/MessageForReadDto.cs
+++ Messager.Chats.Application.Services/DataTransferObjects/MessageForReadDto.cs
@@ -10,7 +10,6 @@ namespace Messager.Chats.Application.Services.DataTransferObjects
     {
         public Guid Id { get; set; }
         public Guid SenderId { get; set; }
-        public string SenderName { get; set; }
         public Guid ChatId { get; set; }
         public string Text { get; set; }
         public DateTime SendingTime { get; set; }
